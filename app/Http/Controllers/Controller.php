c755e37db3596f2884390c0fd408337f077a5d77
Joseph Silber <contact@josephsilber.com>
2023-07-31 09:36:59 -0400

    Create failing test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\LazyCollection;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function test(): LazyCollection
+    {
+        return LazyCollection::times(5)->flatMap(function ($number) {
+            return [$number];
+        });
+    }
 }
